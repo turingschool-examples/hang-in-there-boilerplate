231de7d3d5d74b008d572e9806269d5dff055a96
Horacio Borrego <hborrego365@gmail.com>
2020-03-24 17:27:13 -0600

    Initial commit

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ var quotes = [
   "Act as if what you do makes a difference. It does.",
   "Success is not final, failure is not fatal: it is the courage to continue that counts.",
   "Never bend your head. Always hold it high. Look the world straight in the eye.",
-  "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+  "What you get by achieving your goals is not as important as what you become  by achieving your goals.",
   "Believe you can and you're halfway there.",
   "When you have a dream, you've got to grab it and never let go.",
   "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
