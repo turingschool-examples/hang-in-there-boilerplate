1b036293f2222ee74075ab8a06065ef2b5ec1b58
Carly Clift <carlymclift@gmail.com>
2020-03-25 21:17:58 -0600

    Change title and quote on main page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // query selector variables go here 👇
 var imageWrapper = document.querySelector('.poster');
+var newTitle = document.querySelector('.poster-title');
+var newQuote = document.querySelector('.poster-quote');
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -112,8 +114,18 @@ window.addEventListener("load", getImage)
 
 // functions and event handlers go here 👇
 function getImage() {
-  imageWrapper.insertAdjacentHTML('afterbegin', `<img class="poster-img" src="${images[0]}" alt="nothin' to see here">`);
+  imageWrapper.insertAdjacentHTML('afterbegin', `<img class="poster-img" src="${images[0]}" alt="nothin to see here">)
 }
+
+newTitle.innerHTML = `
+  <h1>believe</h1>
+`;
+
+newQuote.innerHTML = `
+  <h3>You must do the things you think you cannot do.</h3>
+`;
+
+
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
