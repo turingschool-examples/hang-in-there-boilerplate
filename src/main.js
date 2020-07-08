12d6898e00e5a2bfea3d1c1d356df625c7d47058
Ian Holladay <holladayian@gmail.com>
2020-07-08 09:22:36 -0600

    Complete displayPoster function

--- src/main.js
+++ src/main.js
@@ -101,29 +101,25 @@ var quotes = [
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
 var savedPosters = [
-  makePoster(
-    "https://i.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
-    "Optimism",
-    "Keep a joyful heart!"
-  )
 ];
 var currentPoster;
 
 // event listeners go here 👇
-window.addEventListener('load', randPoster);
+window.onload = randPoster; 
 // functions and event handlers go here 👇
 function randPoster() {
-  var randImg = image[getRandomIndex(image)];
-  var randTitle = title[getRandomIndex(title)];
+  var randImg = images[getRandomIndex(images)];
+  var randTitle = titles[getRandomIndex(titles)];
   var randQuote = quotes[getRandomIndex(quotes)];
   currentPoster = new Poster(randImg, randTitle, randQuote);
   displayPoster(currentPoster);
 }
 
 function displayPoster(poster) {
-  posterImg.setAttribute('src', poster.image);
-  posterTitle = poster.title;
-  posterQuote = poster.quote
+  console.log(poster.image);
+  posterImg.setAttribute('src', poster.imageURL);
+  posterTitle.innerText = poster.title;
+  posterQuote.innerText = poster.quote
 }
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
