c1aeaa66b4e2f7f415513612d391ee86a1c682da
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-07-09 19:10:44 -0600

    Add query selector variables

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 // query selector variables go here 👇
+var image = document.querySelector(".poster-img")
+var title = document.querySelector(".poster-title")
+var quote = document.querySelector(".poster-quote")
+
 
 // we've provided you with some data to work with 👇
 var images = [
@@ -98,20 +102,13 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
-var savedPosters = [
-  makePoster(
-    "https://i.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
-    "Optimism",
-    "Keep a joyful heart!"
-  )
-];
-var currentPoster;
+var savedPosters = [];
 
 // event listeners go here 👇
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
