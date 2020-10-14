21a1aad89ce364bcfc0c585164813687b3ba52a0
Christina Cook <cmc.cook7@gmail.com>
2020-10-14 14:34:15 -0600

    Rerandomize image

--- src/main.js
+++ src/main.js
@@ -115,4 +115,4 @@ function getRandomIndex(array) {
 
 title.innerText = titles[getRandomIndex(titles)];
 quote.innerText = quotes[getRandomIndex(quotes)];
-image.src = images[2];
+image.src = images[getRandomIndex(images)];
