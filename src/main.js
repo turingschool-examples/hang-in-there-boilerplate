6b465c79e36c1acbaaac05e9eb4873cab4f29994
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-17 14:59:24 -0600

    Rename showSavedForm function to showSavedView for clarity

--- src/main.js
+++ src/main.js
@@ -134,7 +134,7 @@ showRandom.addEventListener('click', getThreeValues)
 // posterForm.addEventListener('click',) //expecting argument that will be a function which hides current view and brings up form
 // showMain.addEventListener('click',) //expecting argument that will be a function which hides current view and returns you to main page
 // backToMain.addEventListener('click',) //expecting the same argument for showMain button click
-showSaved.addEventListener('click', showSavedForm); //expecting argument that will be a function that shows saved posters view and hides current view
+showSaved.addEventListener('click', showSavedView); //expecting argument that will be a function that shows saved posters view and hides current view
 
 // functions and event handlers go here
 window.onload = getThreeValues();
@@ -152,7 +152,7 @@ function getThreeValues() {
   posterQuote.innerText = currentPoster.quote
 }
 
-function showSavedForm() {
+function showSavedView() {
   mainPoster.className = 'main-poster hidden';
   savedView.className = 'saved-posters';
 }
