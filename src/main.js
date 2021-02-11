e1a85fe7c759efddc8876b939cbe772394b7f307
Zach Johnson <zachjjohns@gmail.com>
2021-02-11 17:31:01 -0600

    Add return to main functionality

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ var mainSection = document.querySelector(".main-poster");
 var formSection = document.querySelector(".poster-form");
 var savedSection = document.querySelector(".saved-posters");
 
+var neverMindButton = document.querySelector(".show-main");
+var backToMainButton = document.querySelector(".back-to-main");
+
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -121,7 +124,8 @@ randomButton.addEventListener('click', function() {
 
 tryItButton.addEventListener('click', showForm);
 savedButton.addEventListener('click', showSaved);
-
+neverMindButton.addEventListener('click', showMain);
+backToMainButton.addEventListener('click', showMain);
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
@@ -153,6 +157,12 @@ function showForm() {
 function showSaved() {
   mainSection.classList.toggle("hidden");
   savedSection.classList.toggle("hidden");
+}
+
+function showMain() {
+  mainSection.classList.toggle("hidden");
+  formSection.classList.add("hidden");
+  savedSection.classList.add("hidden");
 }
 
 randomizeTitle();
