63b4c93c7eb29e00ef1040e6e6b74b8d3b00d1cc
Mae Duphorne <maelduphorne@gmail.com>
2021-05-19 20:42:29 -0600

    Create functionality to generate random poster upon page load

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 // query selector variables go here 👇
-
+var title = document.querySelector(".poster-title");
+var quote = document.querySelector(".poster-quote");
+var image = document.querySelector(".poster-img");
 
 // event listeners go here 👇
+
+window.addEventListener('load', generatePoster);
+
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+
+
+function generatePoster() {
+  image.src = images[getRandomIndex(images)];
+  title.innerText = titles[getRandomIndex(titles)];
+  quote.innerText = quotes[getRandomIndex(quotes)];
+};
