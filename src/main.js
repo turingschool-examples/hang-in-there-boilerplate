91cf5033147eba2b60b1b17509342beab1356972
Mae Duphorne <maelduphorne@gmail.com>
2021-05-20 16:36:40 -0600

    Add functionalist to make your own poster button

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@
 var title = document.querySelector(".poster-title");
 var quote = document.querySelector(".poster-quote");
 var image = document.querySelector(".poster-img");
-var showRandomPosterBtn = document.querySelector(".show-random")
-// event listeners go here 👇
+var showRandomPosterBtn = document.querySelector(".show-random");
+var makeYourOwnPosterBtn = document.querySelector(".show-form");
+var formPage = document.querySelector(".poster-form");
+var mainPage = document.querySelector(".main-poster");
 
+// event listeners go here 👇
 window.addEventListener("load", generatePoster);
 showRandomPosterBtn.addEventListener("click", generatePoster);
+makeYourOwnPosterBtn.addEventListener("click", showFormPage);
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
@@ -14,10 +18,13 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-
-
 function generatePoster() {
   image.src = images[getRandomIndex(images)];
   title.innerText = titles[getRandomIndex(titles)];
   quote.innerText = quotes[getRandomIndex(quotes)];
 };
+
+function showFormPage() {
+  mainPage.classList.add("hidden");
+  formPage.classList.remove("hidden");
+}
