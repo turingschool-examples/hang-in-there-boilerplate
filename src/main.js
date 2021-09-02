071dccac1d0d0120b2ba116e77e7b7969b24c272
Cesare Gallo <cesaregallo0@gmail.com>
2021-09-02 09:22:17 -0600

    Add query selectors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 // query selector variables go here 👇
+var posterImage = document.querySelector('.poster-img');
+var posterTitle = document.querySelector('.poster-title');
+var posterQuote = document.querySelector('.poster-quote');
 
 // we've provided you with some data to work with 👇
 var images = [
@@ -105,7 +108,7 @@ var currentPoster;
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
