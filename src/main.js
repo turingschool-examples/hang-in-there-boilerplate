d843ee338b207b6448e7f14c4d14d76f27d9817c
Kristy Nguyen <knyen008@gmail.com>
2022-04-01 21:28:23 -0600

    Refactor randomizePoster function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// var Poster = require('./poster');
 // query selector variables go here 👇
 //poster elements👇
 var posterImg = document.querySelector(".poster-img");
@@ -119,13 +118,13 @@ var quotes = [
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
 var savedPosters = [];
-var currentPoster;
+var currentPoster = new Poster('url', 'title', 'quote');
 
 // event listeners go here 👇
 window.addEventListener('load', randomizePoster);
 randomButton.addEventListener('click', randomizePoster);
-makePosterButton.addEventListener('click', displayFormView);
 savedPosterButton.addEventListener('click', showSavedPoster);
+makePosterButton.addEventListener('click', displayFormView);
 nevermindButton.addEventListener('click', returnToMain);
 backToMainButton.addEventListener('click', returnToMain);
 showMyPosterButton.addEventListener('click', submitPosterInfo);
@@ -137,19 +136,19 @@ function getRandomIndex(array) {
 };
 
 function randomizePoster() {
-  posterImg.src = images[getRandomIndex(images)];
-  posterTitle.innerText = titles[getRandomIndex(titles)];
-  posterQuote.innerText = quotes[getRandomIndex(quotes)];
+  var image = images[getRandomIndex(images)];
+  var title = titles[getRandomIndex(titles)];
+  var quote = quotes[getRandomIndex(quotes)];
+  currentPoster.updatePosterInfo(image, title, quote);
+  initializePoster();
 };
 
-//Is there a way we can see which button was pressed to make these two functions
-//one function 👇 ?
-function submitPosterInfo(){
+function submitPosterInfo() {
   event.preventDefault();
   getUserInput();
 };
 
-function getUserInput(){
+function getUserInput() {
   var imageUrl = inputUrl.value;
   var title = inputTitle.value;
   var quote = inputQuote.value;
@@ -168,7 +167,7 @@ function addToArray(imageUrl, title, quote) {
   images.push(imageUrl);
   titles.push(title);
   quotes.push(quote);
-}
+};
 
 function showSavedPoster() {
   mainPoster.classList.add("hidden");
@@ -182,9 +181,9 @@ function displayFormView() {
 
 function returnToMain() {
   mainPoster.classList.remove("hidden");
-    if(!posterForm.classList.contains("hidden")){
-        posterForm.classList.add("hidden");
-        } else if(!savedPosterPage.classList.contains("hidden")){
-          savedPosterPage.classList.add("hidden");
-        }
+  if(!posterForm.classList.contains("hidden")){
+    posterForm.classList.add("hidden");
+  } else if(!savedPosterPage.classList.contains("hidden")){
+    savedPosterPage.classList.add("hidden");
+  }
 };
