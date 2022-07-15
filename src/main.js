e2e3d95e89d006899e040de3e138dbafa2f8ce23
Alex Osgood <alex.osgood@gmail.com>
2022-07-14 19:11:21 -0600

    Update

--- src/main.js
+++ src/main.js
@@ -105,7 +105,11 @@ var currentPoster;
 
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
+
+
+
+
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
