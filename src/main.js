8826379df73a32562a43d6a62ee8b5511a43c03f
Hannah.Celemen <hcelemen@icloud.com>
2022-07-15 18:17:03 -0600

    Revert html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // query selector variables go here 👇
 var newPoster = document.querySelector('.poster');
 var showRandomButton = document.querySelector('.show-random');
-
+var createNewPoster = document.querySelector('.show-form');
+var form = document.querySelector('form');
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -105,20 +106,12 @@ var currentPoster;
 
 // event listeners go here 👇
 showRandomButton.addEventListener("click", getRandomPoster)
+createNewPoster.addEventListener("click", goToCreateForm)
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
-var randomTitle = titles[getRandomIndex(titles)];
-var randomQuote = quotes[getRandomIndex(quotes)];
-var randomImage = images[getRandomIndex(images)];
-
-newPoster.innerHTML = `<img class="poster-img" src="${randomImage}" alt="this is a poster">
-  <h1 class="poster-title">${titles[getRandomIndex(titles)]}</h1>
-  <h3 class="poster-quote">${randomQuote}</h3>`;
-
 
 function getRandomPoster() {
   newPoster.innerHTML = `<img class="poster-img" src="${images[getRandomIndex(images)]}" alt="nothin' to see here">
@@ -126,6 +119,8 @@ function getRandomPoster() {
     <h3 class="poster-quote">${quotes[getRandomIndex(quotes)]}</h3>`;
 }
 
-//newPoster.innerHTML = `<img class="poster-img" src="${randomImage}" alt="nothin' to see here">
-//  <h1 class="poster-title">${randomTitle}</h1>
-//  <h3 class="poster-quote">${randomQuote}</h3>`;
+getRandomPoster()
+
+function goToCreateForm() {
+  newPoster.className.add = hidden
+}
