3dbadb93abb864d34faeabeafc2f32613c90d969
Sara Park <sspark106@gmail.com>
2023-02-08 16:00:39 -0700

    feat: Add event listeners to buttons to change to main poster view

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ var form = document.querySelector('.poster-form');
 var mainPosterView = document.querySelector('.main-poster');
 var showSavedBtn = document.querySelector('.show-saved');
 var savedPostersView = document.querySelector('.saved-posters');
+var takeMeBackBtn = document.querySelector('.show-main');
+var backToMainBtn = document.querySelector('.back-to-main');
 
 // we've provided you with some data to work with 👇
 var images = [
@@ -115,6 +117,8 @@ window.addEventListener("load", loadHomePage);
 showRandomBtn.addEventListener("click", loadHomePage);
 makeYourOwnBtn.addEventListener("click", showForm);
 showSavedBtn.addEventListener("click", showSaved)
+takeMeBackBtn.addEventListener("click", showMain);
+backToMainBtn.addEventListener("click", showMain);
 
 // functions and event handlers go here 👇
 function loadHomePage() {
@@ -141,6 +145,12 @@ function showForm() {
 function showSaved() {
   savedPostersView.classList.remove("hidden");
   mainPosterView.classList.add("hidden");
+}
+
+function showMain() {
+  form.classList.add("hidden");
+  savedPostersView.classList.add("hidden");
+  mainPosterView.classList.remove("hidden");
 }
 
 // (we've provided one for you to get you started)!
