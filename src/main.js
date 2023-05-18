02d829f1d01af789d6fc9d670de7b455eb6efebd
Alex Peterson <aspeterson.126@gmail.com>
2023-05-17 19:23:31 -0500

    Initial commit

--- src/main.js
+++ src/main.js
@@ -99,7 +99,6 @@ var quotes = [
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
 var savedPosters = [];
-var currentPoster;
 
 // event listeners go here 👇
 
