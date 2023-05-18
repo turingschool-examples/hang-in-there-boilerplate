23cbaeccecbf303718079aca08f8f034d0651087
Josh Bennett <joshbennett793@yahoo.com>
2023-05-17 22:48:11 -0500

    Call render poster functionality on window load

--- src/main.js
+++ src/main.js
@@ -107,6 +107,8 @@ var currentPoster;
 
 // event listeners go here 👇
 
+window.addEventListener('load', renderRandomPoster);
+
 // functions and event handlers go here 👇
 // (we've provided two to get you started)!
 function getRandomIndex(array) {
@@ -156,6 +158,8 @@ function changePosterQuote(quote) {
 	posterQuote.innerText = quote;
 }
 
-changePosterImg(randomPoster.imageURL);
-changePosterTitle(randomPoster.title);
-changePosterQuote(randomPoster.quote);
+function renderRandomPoster() {
+	changePosterImg(randomPoster.imageURL);
+	changePosterTitle(randomPoster.title);
+	changePosterQuote(randomPoster.quote);
+}
