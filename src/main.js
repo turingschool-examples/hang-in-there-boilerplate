1d2a6f4b470816012cb949d673d9d33dd8c8a489
Seong Kang <seong.h.kang2@gmail.com>
2024-03-27 16:57:26 -0500

    Added query selectors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 // query selector variables go here 👇
+var posterImage = document.querySelector('.poster-img')
+var posterTitle = document.querySelector('.poster-title')
+var posterQuote = document.querySelector('.poster-quote')
+
+console.log(posterImage)
 
 // we've provided you with some data to work with 👇
 var images = [
@@ -98,11 +103,11 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
+
 var savedPosters = [];
 var currentPoster;
 
 // event listeners go here 👇
-
 // functions and event handlers go here 👇
 // (we've provided two to get you started)!
 function getRandomIndex(array) {
