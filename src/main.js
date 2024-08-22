b872b5af44724ac77c0e1ffed4871913dedc7fd8
Terra Manning <terradmanning@gmail.com>
2024-08-22 07:32:06 -0600

    feat: copy and paste the un-motivational poster data

--- src/main.js
+++ src/main.js
@@ -97,6 +97,131 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
+
+// un-motivational poster data
+const unmotivationalPosters = [
+  {
+    name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url: "./assets/failure.jpg",
+  },
+  {
+    name: "MEDIOCRITY",
+    description: "Dreams are just that—dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url: "./assets/mediocrity.jpg",
+  },
+  {
+    name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "./assets/regret.jpg",
+  },
+  {
+    name: "FUTILITY",
+    description: "You're not good enough.",
+    price: 150.00,
+    year: 2016,
+    vintage: false,
+    img_url:  "./assets/futility.jpg",
+  },
+  {
+    name: "DEFEAT",
+    description: "It's too late to start now.",
+    price: 35.00,
+    year: 2023,
+    vintage: false,
+    img_url:  "./assets/defeat.jpg",
+  },
+  {
+    name: "HOPELESSNESS",
+    description: "Stay in your comfort zone; it's safer.",
+    price: 112.00,
+    year: 2020,
+    vintage: true,
+    img_url: "./assets/hopelessness.jpg",
+  },
+  {
+    name: "LAZINESS",
+    description: "You can't change anything.",
+    price: 25.00,
+    year: 2022,
+    vintage: false,
+    img_url: "./assets/laziness.jpg",
+  },
+  {
+    name: "PROCRASTINATION",
+    description: "Better to avoid failure by not trying at all.",
+    price: 48.00,
+    year: 2017,
+    vintage: true,
+    img_url: "./assets/procrastination.jpg",
+  },
+  {
+    name: "DESPAIR",
+    description: "Let someone else do it; you’ll just mess it up.",
+    price: 73.00,
+    year: 2015,
+    vintage: false,
+    img_url: "./assets/despair.jpg",
+  },
+  {
+    name: "NEGLECT",
+    description: "Happiness is overrated.",
+    price: 160.00,
+    year: 2019,
+    vintage: true,
+    img_url: "./assets/neglect.jpg",
+  },
+  {
+    name: "FEAR",
+    description: "Giving up is always an option.",
+    price: 91.00,
+    year: 2014,
+    vintage: false,
+    img_url: "./assets/fear.jpg",
+  },
+  {
+    name: "APATHY",
+    description: "No one cares about your effort.",
+    price: 110.00,
+    year: 2016,
+    vintage: true,
+    img_url: "./assets/apathy.jpg",
+  },
+  {
+    name: "MISERY",
+    description: "Why take risks when you can stay stagnant?",
+    price: 55.00,
+    year: 2021,
+    vintage: false,
+    img_url: "./assets/misery.jpg",
+  },
+  {
+    name: "BLAME",
+    description: "Expect disappointment and you'll never be disappointed.",
+    price: 39.00,
+    year: 2017,
+    vintage: true,
+    img_url: "./assets/blame.jpg",
+  },
+  {
+    name: "DOUBT",
+    description: "Success is for other people, not you.",
+    price: 140.00,
+    year: 2020,
+    vintage: false,
+    img_url: "./assets/doubt.jpg",
+  }
+];
+
 // query selector variables go here 👇
 var posterImage = document.querySelector(".poster-img");
 var posterTitle = document.querySelector(".poster-title");
@@ -113,14 +238,13 @@ var showMainPoster = document.querySelector(".main-poster");
 var savedPostersPage = document.querySelector(".saved-posters");
 var savedPostersGrid = document.querySelector(".saved-posters-grid");
 var unmotivationalBackToMainButton = document.querySelector("#unmotivational-back-to-main")
+var unmotivationalPostersView = document.querySelector(".unmotivational-posters");
+var unmotivationalPostersGrid = document.querySelector(".unmotivational-posters-grid");
+var unmotivationalPostersButton = document.querySelector(".show-unmotivational");
 
 var userImage = document.querySelector("#poster-image-url");
 var userTitle = document.querySelector("#poster-title");
 var userQuote = document.querySelector("#poster-quote");
-
-var unmotivationalPostersView = document.querySelector(".unmotivational-posters");
-var unmotivationalPostersGrid = document.querySelector(".unmotivational-posters-grid");
-var unmotivationalPostersButton = document.querySelector(".show-unmotivational")
 
 //global variables
 var savedPosters = [];
@@ -136,12 +260,8 @@ showMainButton.addEventListener("click", backToMainPage);
 backToMainButton.addEventListener("click", backToMainPage);
 makePosterButton.addEventListener("click", showUserPoster);
 saveButton.addEventListener("click", saveGeneratedPoster);
-unmotivationalPostersButton.addEventListener("click", showUnmotivationalPosters)
-unmotivationalBackToMainButton.addEventListener("click", backToMainFromUnmotivational)
-// backToMainButton.addEventListener("click", function() {
-//   backToMainPage()
-//   showUnmotivationalPosters()
-// });
+unmotivationalPostersButton.addEventListener("click", showUnmotivationalPosters);
+unmotivationalBackToMainButton.addEventListener("click", backToMainFromUnmotivational);
 
 // functions and event handlers go here 👇
 
@@ -180,12 +300,6 @@ function showSavedPostersView() {
   savedPostersPage.classList.remove("hidden");
   showMainPoster.classList.add("hidden");
 }
-
-// function backToMainPage() {
-//   posterForm.classList.add("hidden");
-//   showMainPoster.classList.remove("hidden");
-//   savedPostersPage.classList.add("hidden");
-// }
 
 function backToMainPage() {
   posterForm.classList.add("hidden");
