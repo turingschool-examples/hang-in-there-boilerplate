5401993e94829f1f93ae50b43966e80097618ea6
Renee Messersmith <morganrmessersmith@gmail.com>
2024-08-28 14:52:53 -0400

    fix: removed comments

--- src/main.js
+++ src/main.js
@@ -28,15 +28,6 @@ var unmotivationalGridDiv = document.querySelector(".poster-grid");
 
 var allButtons = document.querySelectorAll("button")
 
-// allButtons.forEach( (button) => {
-//   button.addEventListener('mousedown', playButtonAudio)
-// })
-
-// function playButtonAudio() {
-//   var audio = new Audio("https://uploads.sitepoint.com/wp-content/uploads/2023/06/1687569402mixkit-fast-double-click-on-mouse-275.wav");
-//   audio.play();
-// }
-
 var images = [
   "./assets/bees.jpg",
   "./assets/bridge.jpg",
